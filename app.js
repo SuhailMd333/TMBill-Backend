c2924fc014ef1598d3bd5f3ef2b2635883b2764a
Suhail Shaikh <suhailshk333@gmail.com>
2025-05-12 05:43:57 +0530

    .

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ const TaskAPI = require('./routes/task')
 
 
 
-app.use('/',(req,res) => {
-    res.send('Hello World!');
+// app.use('/',(req,res) => {
+//     res.send('Hello World!');
 
-})
+// })
 
 app.use('/api/v1', UserAPI)
 app.use('/api/v2', TaskAPI)
