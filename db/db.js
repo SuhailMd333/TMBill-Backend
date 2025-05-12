c530280d58453bd6527d3ab750fb0502535ac51b
Suhail Shaikh <suhailshk333@gmail.com>
2025-05-12 06:00:18 +0530

    .

--- db/db.js
+++ db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');   
 const db = async () => {
     try {
-        const response =  await mongoose.connect(`${process.env.MONGODB_URI || "mongodb+srv://suhailshk333:pYKuQOHexm5PJHYE@cluster0.ltgwj4r.mongodb.net/task-management"}`)
+        const response =  await mongoose.connect("mongodb+srv://suhailshk333:pYKuQOHexm5PJHYE@cluster0.ltgwj4r.mongodb.net/task-management")
         if(response){
             console.log('Database connected successfully');
         }
